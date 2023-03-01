1bb55352a912ced7dc025761f7df00a6e66a75b9
Kolystir <Kolystir@yandex.com>
2023-03-01 09:08:43 +0300

    1-5

---
+++ Solution_1-5/Array1/Array1.cs
@@ -0,0 +1,17 @@
+﻿// Дано целое число N (> 0). Сформировать и вывести целочисленный массив размера N,
+// содержащий N первых положительных нечетных чисел: 1, 3, 5, … .
+Console.Write("Введите количество чисел в массиве: ");
+int massiv = Convert.ToInt32(Console.ReadLine());
+int[] main = new int[massiv];
+int a = 1;
+for (int i = 0; i < massiv; i++)
+{
+    main[i] = a;
+    a = a + 2;
+}
+Console.WriteLine("Вот ваши числа в массиве: ");
+for (int i = 0; i < massiv; i++)
+{
+    Console.Write(main[i]);
+    Console.Write(" ");
+}
---
+++ Solution_1-5/Array6/Array6.cs
@@ -0,0 +1,33 @@
+﻿// Даны целые числа N (> 2), A и B. Сформировать и вывести целочисленный массив размера N, первый элемент которого равен A, второй равен B, а каждый последующий элемент равен сумме всех предыдущих.
+Console.Write("Введите переменную N > 2: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int f = 2;
+int c = a;
+int d = b;
+int e = 0;
+if (n > 2)
+{
+
+    int[] posledovatelnost = new int[n];
+    for (int i = 0; i < n - 2; i++)
+    {
+
+        e += a + b;
+        posledovatelnost[i] = e;
+    }
+    Console.WriteLine($"1 массив = {c}");
+    Console.WriteLine($"2 массив = {d}");
+    for (int i = 0; i < n - 2; i++)
+    {
+        Console.WriteLine($"{f + 1} массив  = " + posledovatelnost[i]);
+    }
+
+}
+else
+{
+    Console.WriteLine("Переменная N не подходи для нешаего условия!");
+}
